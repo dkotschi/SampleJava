acc5ec774a51e3c5501776d8b77649cdbf51d704
david <david.kotschenreuther@gmail.com>
2026-03-16 15:57:45 +0100

    even method comment added

--- code/src/main/java/Service/EmailClient.java
+++
@@ -1,6 +0,0 @@
-package Service;
-
-public interface EmailClient {
-
-    EmailResponse send(String address, String message);
-}
--- code/src/main/java/Service/EmailResponse.java
+++
@@ -1,3 +0,0 @@
-package Service;
-
-public record EmailResponse(boolean success) {}
